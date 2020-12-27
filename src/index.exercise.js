a7b7350485bdcd29e22b304db87f341343157395
Ryan Kienstra <kienstraryan@gmail.com>
2020-12-27 14:01:42 -0600

    Simplify name of state to modalDisplaying

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -9,27 +9,27 @@ import {LoginButton} from 'components/login-button'
 import { RegisterButton } from 'components/register-button'
 
 function App() {
-  const possibleModalDisplays = {
+  const possibleOpenModals = {
     login: 'login',
     register: 'register',
     none: 'none',
   }
 
-  const [modalDisplaying, setModalDisplaying] = React.useState(possibleModalDisplays.none)
-  const closeDialog = () => setModalDisplaying(possibleModalDisplays.none)
+  const [openModal, setOpenModal] = React.useState(possibleOpenModals.none)
+  const closeDialog = () => setOpenModal(possibleOpenModals.none)
 
   return (
     <>
       <Logo />
       <h1>Bookshelf</h1>
       <div>
-        <LoginButton onClick={ () => setModalDisplaying(possibleModalDisplays.login) } />
+        <LoginButton onClick={ () => setOpenModal(possibleOpenModals.login) } />
       </div>
       <div>
-        <RegisterButton onClick={ () => setModalDisplaying(possibleModalDisplays.register) }/>
+        <RegisterButton onClick={ () => setOpenModal(possibleOpenModals.register) }/>
       </div>
       <Dialog
-        isOpen={modalDisplaying === possibleModalDisplays.login}
+        isOpen={openModal === possibleOpenModals.login}
         onDismiss={closeDialog}
         aria-label="Log in for app"
       >
@@ -40,7 +40,7 @@ function App() {
         <p>Please log in</p>
       </Dialog>
       <Dialog
-        isOpen={modalDisplaying === possibleModalDisplays.register}
+        isOpen={openModal === possibleOpenModals.register}
         onDismiss={closeDialog}
         aria-label="Register for app"
       >
