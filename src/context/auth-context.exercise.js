b2d20421a06778a345b88aea92bacdec29db3bf1
Ryan Kienstra <kienstraryan@gmail.com>
2021-01-12 11:15:53 -0600

    Use Kent's suggestions for EC 2
    Use conditional of
    if (context === undefined)
    instead of if (

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -3,8 +3,8 @@ import * as React from 'react'
 const AuthContext = React.createContext()
 const useAuth = () => {
   const context = React.useContext(AuthContext);
-  if (!context) {
-    throw new Error('The useAuth() hook must be called inside AuthContext.Provider')
+  if (context === undefined) {
+    throw new Error('useAuth must be used inside AuthContext.Provider')
   }
 
   return context
