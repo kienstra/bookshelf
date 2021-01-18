623f1318b80317f2c8240eb5ef4f0788d1b2a714
Ryan Kienstra <kienstraryan@gmail.com>
2021-01-17 23:40:23 -0600

    Main exercise of unit testing
    Tests for formatDate()
    and client().

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,11 @@
-test.todo('formatDate formats the date to look nice')
+import {formatDate} from 'utils/misc'
 
+describe('formatDate formats the date to look nice', () => {
+  it.each([
+    [0, 'Dec 69'],
+    ['', 'Dec 69'],
+    [1610944527117, 'Jan 21'],
+  ])('produces the correct formatting', (date, expected) => {
+    expect(formatDate(date)).toStrictEqual(expected)
+  })
+})
