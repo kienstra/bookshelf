bf5f675a2b30bd4aeeb632acb9bc56d77fb176a8
Ryan Kienstra <kienstraryan@gmail.com>
2021-01-31 16:06:58 -0600

    Extra credit 5: add more test cases
    As Kent suggested,
    test that you can remove a list item,
    mark one as read,
    and edit a note.

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import{render, screen, userEvent, waitForLoadingToFinish} from 'test/app-test-utils'
+import{render, screen, userEvent, waitForElementToBeRemoved, waitForLoadingToFinish} from 'test/app-test-utils'
 import * as booksDB from 'test/data/books'
 import {buildBook} from 'test/generate'
 import {formatDate} from 'utils/misc'
@@ -55,4 +55,49 @@ test('can create a list item for the book', async () => {
   expect(screen.getByLabelText(/start date/i)).toHaveTextContent(formatDate(new Date()))
   expect(screen.getByLabelText(/notes/i)).toBeInTheDocument()
   expect(screen.queryByRole('radio',{name: /star/i})).not.toBeInTheDocument()
+})
+
+test('can remove a list item for the book', async () => {
+  const book = await booksDB.create(buildBook())
+  const route = `/book/${book.id}`
+  await render(<App />, {route})
+
+  const addToListButton = screen.getByRole('button', {name: /add to list/i})
+  userEvent.click(addToListButton)
+  await waitForLoadingToFinish()
+
+  userEvent.click(screen.getByRole('button', {name: /remove from list/i}))
+  await waitForLoadingToFinish()
+  expect(screen.getByRole('button', {name: /add to list/i})).toBeInTheDocument()
+})
+
+test('can mark a list item as read', async () => {
+  const book = await booksDB.create(buildBook())
+  const route = `/book/${book.id}`
+  await render(<App />, {route})
+
+  userEvent.click(screen.getByRole('button', {name: /add to list/i}))
+  await waitForLoadingToFinish()
+
+  userEvent.click(screen.getByRole('button', {name: /mark as read/i}))
+  await waitForLoadingToFinish()
+
+  screen.getAllByRole('radio',{name: /star/i})
+  expect(screen.queryByRole('button', {name: /mark as read/i})).not.toBeInTheDocument()
+  expect(screen.queryByRole('button', {name: /mark as unread/i})).toBeInTheDocument()
+})
+
+test('can edit a note', async () => {
+  const book = await booksDB.create(buildBook())
+  const route = `/book/${book.id}`
+  await render(<App />, {route})
+
+  userEvent.click(screen.getByRole('button', {name: /add to list/i}))
+  await waitForLoadingToFinish()
+
+  const noteBox = screen.getByRole('textbox', {name: /notes/i})
+  const noteText = 'Here is an example note'
+  userEvent.type(noteBox, noteText)
+
+  expect(noteBox).toHaveValue(noteText)
 })
